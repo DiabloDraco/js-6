b6e9482cf16dd9a2132e4c5f765477c5c6488249
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2022-07-09 21:17:50 +0500

    bug fix

--- js/main.js
+++ js/main.js
@@ -1,23 +1,24 @@
-var elTempForm = document.querySelector('.temp__form');
-var elTempOutput = document.querySelector('.temp__output');
+var elTempForm = document.querySelector('.form');
+var elTempOutput = document.querySelector('.output');
 
 elTempForm.addEventListener(`submit` , function (evt) {
-    evt.preventDefault();
-
-    var elTempInput = parseFloat(document.querySelector('.temp__input').value.trim(), 10)
-
-    if (isNaN = (elTempInput)) {
-        var tempReslut = (elTempInput * (9/5) + 32).toFixed(1)
-        elTempOutput.value = tempReslut;
-    }else {
-        elTempOutput.value = ('Pls type a number')
-    }
-
-    if (elTempInput < 0) {
-        document.body.classList = ('bg-cold');
-      } else if (elTempInput < 20) {
-        document.body.classList = ('bg-normal');
-      } else {
-        document.body.classList = ('bg-hot');
-      }
+  evt.preventDefault();
+  
+  var elTempInput = parseFloat(document.querySelector('.input').value.trim(), 10)
+  
+  if (isNaN = (elTempInput)) {
+    var Reslut = (elTempInput * (9/5) + 32).toFixed(1)
+    elTempOutput.value = Reslut;
+  }
+  else {
+    elTempOutput.value = ('Pls type a number')
+  }
+  
+  if (elTempInput < 0) {
+    document.body.classList = ('bg-cold');
+  } else if (elTempInput < 20) {
+    document.body.classList = ('bg-normal');
+  } else {
+    document.body.classList = ('bg-hot');
+  }
 })
